0f96514acc657b42f35088689d9d3f245e8fff37
josemigu <josemigu@student.42porto.com>
2025-12-09 12:56:08 +0000

    after initial merge, added w component in tuple_magnitude and tuple_normalize

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: josemigu <josemigu@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/06/06 15:17:48 by josemigu          #+#    #+#              #
-#    Updated: 2025/12/08 14:44:11 by josemigu         ###   ########.fr        #
+#    Updated: 2025/12/09 12:48:31 by josemigu         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -47,23 +47,9 @@ ifeq ($(UNAME_S),Darwin)
 	CFLAGS_EXTRA = -L/opt/X11/lib -lX11 -lXext -lXrandr -lXcursor -lXinerama -lXxf86vm -lXrender -lX11-xcb -lXfixes -lm
 endif
 
-<<<<<<< HEAD
 # Sources and Objects
 SRCS		= $(wildcard $(SRC_DIR)/*/*.c) $(wildcard $(SRC_DIR)/*.c)
 OBJS		= $(patsubst $(SRC_DIR)/%,$(OBJ_DIR)/%,$(SRCS:.c=.o))
-=======
-# Sources
-SRC_PATH	=	srcs/
-SRC			=	miniRT.c \
-				parser/parser.c \
-				parser/parse_objects.c \
-				parser/parse_objects2.c \
-				parser/parse_elements.c \
-				parser/parse_utils.c \
-				parser/parse_utils2.c \
-				utils/scene_utils.c
-SRCS		= $(addprefix $(SRC_PATH), $(SRC))
->>>>>>> main
 
 # Libraries
 LIBFT		= $(LIBFT_DIR)/libft.a
--- srcs/algebra/operations.c
+++ srcs/algebra/operations.c
@@ -1,20 +1,8 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   operations.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mreinald <mreinald@student.42porto.com>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/08 22:48:29 by mreinald          #+#    #+#             */
-/*   Updated: 2025/12/08 22:48:40 by mreinald         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <algebra.h>
 
 double	tuple_magnitude(t_tuple v)
 {
-	return (sqrt(v.x * v.x + v.y * v.y + v.z * v.z));
+	return (sqrt(v.x * v.x + v.y * v.y + v.z * v.z + v.w * v.w));
 }
 
 t_tuple	*tuple_normalize(t_tuple *v)
@@ -29,10 +17,12 @@ t_tuple	*tuple_normalize(t_tuple *v)
 		result->x = 0.0;
 		result->y = 0.0;
 		result->z = 0.0;
+		result->w = 0.0;
 		return (result);
 	}
 	result->x = v->x / mag;
 	result->y = v->y / mag;
 	result->z = v->z / mag;
+	result->w = v->w / mag;
 	return (result);
 }
