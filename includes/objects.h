ea85935c66b708a3e2c9a82a0063ccc79524a581
Matheus Diniz <mreinald@student.42porto.com>
2026-03-04 15:39:50 +0000

    includes for objects

---
+++ includes/objects.h
@@ -0,0 +1,19 @@
+#ifndef OBJECTS_H
+#define OBJECTS_H
+
+#include <structs.h>
+
+// Sphers
+
+t_sphere	*create_sphere(t_tuple center, double diameter, t_color color);
+t_material	default_material(void);
+void		set_sphere_transform(t_sphere *sphere, t_matrix *transform);
+
+
+// Cylindre
+
+
+//Plane
+
+
+#endif
